d46b3afbdeceaa729902e703f65985e02d51beef
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-27 11:09:56 +0300

    feat: add initial home page

---
+++ lib/views/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white10,
+     body: Center(
+      child: Text(
+        'Home Screen'
+      ),
+    )
+    );
+  }
+}
