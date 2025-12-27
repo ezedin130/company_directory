7ff8d6570d17ed4dc5c17d0fe7824ffb1b20c73b
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-27 11:45:19 +0300

    feat: make CompanyCard tappable

--- lib/widgets/company_card.dart
+++ lib/widgets/company_card.dart
@@ -2,28 +2,33 @@ import 'package:flutter/material.dart';
 
 class CompanyCard extends StatelessWidget {
   final String companyName;
+  final Function()? onTap;
 
   const CompanyCard({
     super.key,
     required this.companyName,
+    required this.onTap
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 80,
-      margin: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Center(
-        child: Text(
-          companyName,
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 80,
+        margin: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Center(
+          child: Text(
+            companyName,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ),
       ),
