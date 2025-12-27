f7ace37d94163894d3a6e201b7524ac45ad38c74
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-27 15:35:10 +0300

    feat: enable toggling favorites from HomePage

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,40 +1,38 @@
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
+import '../controller/favorite_controller.dart';
 import '../widgets/company_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   final List<String> companies = const [
-    'Google',
-    'Apple',
-    'Microsoft',
-    'Amazon',
-    'Meta',
-    'Netflix',
-    'Tesla',
-    'IBM',
-    'Intel',
-    'Adobe',
+    'Google', 'Apple', 'Microsoft', 'Amazon', 'Meta',
+    'Netflix', 'Tesla', 'IBM', 'Intel', 'Adobe',
   ];
 
   @override
   Widget build(BuildContext context) {
+    final FavoritesController favoritesController = Get.find();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Company Directory'),
-      ),
+      appBar: AppBar(title: const Text('Company Directory')),
       body: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: companies.length,
-          itemBuilder: (BuildContext context, int index) {
-            return CompanyCard(
-              companyName: companies[index],
-              onTap: (){
-                print('${companies[index]} tapped');
+        padding: const EdgeInsets.all(8),
+        itemCount: companies.length,
+        itemBuilder: (context, index) {
+          final company = companies[index];
+          return Obx(() => CompanyCard(
+            companyName: company,
+            isFavorite: favoritesController.isFavorite(company),
+            onFavoriteTap: () {
+              favoritesController.toggleFavorite(company);
+            },
+            onTap: () {
+
               },
-            );
-          }
-      )
+          ));
+        },
+      ),
     );
   }
 }
