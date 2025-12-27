113c01e954a03add0c161cdddc3d737996ab38a6
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-27 15:27:38 +0300

    feat: implement main navigation shell with GetX and BottomNavigationBar

---
+++ lib/widgets/navigation_wrapper.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controller/navigation_controller.dart';
+import '../views/favorites_page.dart';
+import '../views/home_page.dart';
+
+class NavigationWrapper extends StatelessWidget {
+  const NavigationWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final NavigationController navController = Get.put(NavigationController());
+
+    final List<Widget> screens = [
+      const HomePage(),
+      const FavoritesPage(),
+    ];
+
+    return Obx(() => Scaffold(
+      body: screens[navController.currentIndex.value],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: navController.currentIndex.value,
+        onTap: navController.changeTab,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorites'),
+        ],
+      ),
+    ));
+  }
+}
+
