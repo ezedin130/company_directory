1c42901d879f6ab2ac5d525331789ee2d5fc803a
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-27 11:38:04 +0300

    feat: display companies using ListView.builder on HomePage

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/company_card.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  final List<String> companies = const [
+    'Google',
+    'Apple',
+    'Microsoft',
+    'Amazon',
+    'Meta',
+    'Netflix',
+    'Tesla',
+    'IBM',
+    'Intel',
+    'Adobe',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white10,
-     body: Center(
-      child: Text(
-        'Home Screen'
+      appBar: AppBar(
+        title: const Text('Company Directory'),
       ),
-    )
+      body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: companies.length,
+          itemBuilder: (BuildContext context, int index) {
+            return CompanyCard(
+              companyName: companies[index],
+            );
+          }
+      )
     );
   }
 }
