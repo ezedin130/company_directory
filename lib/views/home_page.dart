a5f7a5afb79640aafd69427910afa515c5ba204c
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-28 13:07:28 +0300

    feat: update home screen to retrieve data from api instead of mock data

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,38 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../controller/company_controller.dart';
 import '../controller/favorite_controller.dart';
 import '../widgets/company_card.dart';
+import 'company_detail_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-  final List<String> companies = const [
-    'Google', 'Apple', 'Microsoft', 'Amazon', 'Meta',
-    'Netflix', 'Tesla', 'IBM', 'Intel', 'Adobe',
-  ];
 
   @override
   Widget build(BuildContext context) {
-    final FavoritesController favoritesController = Get.find();
+    final CompanyController companyController = Get.put(CompanyController());
+    final FavoritesController favoritesController = Get.put(FavoritesController());
 
     return Scaffold(
       appBar: AppBar(title: const Text('Company Directory')),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: companies.length,
-        itemBuilder: (context, index) {
-          final company = companies[index];
-          return Obx(() => CompanyCard(
-            companyName: company,
-            isFavorite: favoritesController.isFavorite(company),
-            onFavoriteTap: () {
-              favoritesController.toggleFavorite(company);
-            },
-            onTap: () {
+      body: Obx(() {
+        if (companyController.isLoading.value) {
+          return const Center(child: CircularProgressIndicator());
+        }
 
+        if (companyController.errorMessage.isNotEmpty) {
+          return Center(child: Text(companyController.errorMessage.value));
+        }
+
+        final companies = companyController.companies;
+        return ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: companies.length,
+          itemBuilder: (context, index) {
+            final company = companies[index];
+
+            return Obx(() => CompanyCard(
+              companyName: company.name,
+              isFavorite: favoritesController.isFavorite(company.name),
+              onTap: () {
+                Get.to(() => CompanyDetailPage(company: company));
               },
-          ));
-        },
-      ),
+              onFavoriteTap: () {
+                favoritesController.toggleFavorite(company.name);
+              },
+            ));
+          },
+        );
+      }),
     );
   }
 }
